997dc856feea093cb499cbf3a218d4016548b97c
Jonathan Robin Langford <jrobinlangford@gmail.com>
2021-02-04 07:38:26 -0600

    Create base django project

---
+++ bank_ddd_demo/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bank_ddd_demo project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bank_ddd_demo.settings')
+
+application = get_asgi_application()
---
+++ bank_ddd_demo/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bank_ddd_demo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bank_ddd_demo.settings')
+
+application = get_wsgi_application()
